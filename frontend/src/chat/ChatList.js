1b4f955264903c90815fbc2f5491d99ba504b0b2
Твоє Ім'я <_@.com>
2025-04-12 15:07:13 +0300

    Added axios dependency

--- frontend/src/chat/ChatList.js
+++ frontend/src/chat/ChatList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './ChatsList.css'; // Імпорт CSS-файлу
+import './ChatsList.css'; // Імпорт CSS-файлуі
 
 const ChatsList = () => {
   const [chats, setChats] = useState([]);
