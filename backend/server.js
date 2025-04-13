f0da3f4c7fe61e9925b0573b014c59a634613bd3
Твоє Ім'я <_@.com>
2025-04-13 11:51:52 +0300

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+// Дозволяємо лише конкретні домени (де твій фронтенд)
+const allowedOrigins = [
+  'https://versel-ashen.vercel.app/', // Замінити на актуальний URL
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // дозволяємо запит
+    } else {
+      callback(new Error('Not allowed by CORS')); // якщо домен не в списку
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Включаємо CORS на сервері
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const MONGO_URI = process.env.MONGO_URI;
@@ -17,7 +36,6 @@ app.use('/api/games', require('./routes/game'));
 app.use('/api/comments', require('./routes/comment'));
 app.use('/api/chats', require('./routes/chat'));
 app.use('/api/library', require('./routes/library'));
-
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
